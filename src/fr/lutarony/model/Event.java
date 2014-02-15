841aadc1b5716eb338ec7f67e167fd20869d59ca
Patryk Gruszka <gruszk_p@etna-alternance.net>
2014-02-15 21:14:12 +0100

    event model add

---
+++ src/fr/lutarony/model/Event.java
@@ -0,0 +1,52 @@
+package fr.lutarony.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "EVENT")
+public class Event {
+
+	private int id;
+	private String name;
+	private int teamId;
+
+	@Id
+	@Column(name = "ID", unique = true, nullable = false)
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	@Column(name = "NAME", nullable = false)
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Column(name = "ID", nullable = false)
+	public int getTeamId() {
+		return teamId;
+	}
+
+	public void setTeamId(int teamId) {
+		this.teamId = teamId;
+	}
+
+	@Override
+	public String toString() {
+		StringBuffer strBuff = new StringBuffer();
+		strBuff.append("id : ").append(getId());
+		strBuff.append(", name : ").append(getName());
+		strBuff.append(", team_id : ").append(getTeamId());
+		return strBuff.toString();
+	}
+}
